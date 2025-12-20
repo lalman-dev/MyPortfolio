cff18489703c4435fd7d6435d81a2f36d9916b15
Lalman <lalmanchaudhary2@gmail.com>
2025-12-20 13:29:56 +0530

    update:data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -13,7 +13,6 @@ import {
   Users,
   User,
   Rocket,
-  ChevronsLeftRightEllipsis,
 } from "lucide-react";
 
 import project1 from "../assets/hackernews.png";
@@ -21,7 +20,14 @@ import project3 from "../assets/interviewapp.png";
 import project2 from "../assets/resumeBuilder.png";
 
 import { FiGithub, FiLinkedin } from "react-icons/fi";
-import { SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
+import {
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiTypescript,
+  SiReact,
+  SiTailwindcss,
+} from "react-icons/si";
 import { VscRocket } from "react-icons/vsc";
 import type { LucideIcon } from "lucide-react";
 import type { IconType } from "react-icons";
@@ -94,68 +100,75 @@ export const SKILL_CATEGORY: SkillCategory[] = [
   {
     title: "Front-end",
     icon: Code2,
-    description: "Crafting beautiful, responsive interfaces",
+    description:
+      "Focused on building modern responsive UIs with React, TypeScript, and TailwindCSS enhanced by smooth animations",
     skills: [
-      { name: "React", level: 95, color: "bg-blue-600" },
-      { name: "TypeScript", level: 85, color: "bg-blue-700" },
-      { name: "NextJS", level: 90, color: "bg-gray-500" },
-      { name: "TailwindCSS", level: 92, color: "bg-cyan-600" },
-      { name: "FramerMotion", level: 82, color: "bg-red-600" },
+      { name: "React", level: 80, color: "bg-blue-600" },
+      { name: "TypeScript", level: 75, color: "bg-blue-700" },
+      { name: "TailwindCSS", level: 85, color: "bg-cyan-600" },
+      { name: "FramerMotion", level: 75, color: "bg-red-600" },
     ],
   },
   {
-    title: "Backend",
+    title: "Tools",
+    icon: Wrench,
+    description:
+      "Proficient with essential development and collaboration tools that streamline coding, design, and teamwork",
+    skills: [
+      { name: "Git", level: 80, color: "bg-orange-600" },
+      { name: "GitHub", level: 85, color: "bg-gray-800" },
+      { name: "VS Code", level: 85, color: "bg-blue-500" },
+      { name: "Figma", level: 75, color: "bg-pink-500" },
+      { name: "Notion", level: 70, color: "bg-gray-500" },
+      { name: "Vercel/Netlify", level: 70, color: "bg-green-500" },
+      { name: "NPM", level: 75, color: "bg-red-500" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: Users,
+    description:
+      "Core strengths that help me contribute effectively in projects and adapt to dynamic environments",
+    skills: [
+      { name: "Problem Solving", level: 85, color: "bg-indigo-600" },
+      { name: "Continuous Learning", level: 90, color: "bg-green-500" },
+      { name: "Communication", level: 80, color: "bg-purple-500" },
+      { name: "Adaptability", level: 78, color: "bg-teal-500" },
+      { name: "Collaboration", level: 75, color: "bg-pink-400" },
+    ],
+  },
+  {
+    title: "Currently learning",
     icon: Server,
     description:
-      "While my primary focus is frontend, I’m also exploring backend tools to understand full‑stack development",
+      "While my primary focus is frontend, I'm also exploring backend and database tools to understand full-stack development",
     skills: [
+      { name: "NextJS", level: 90, color: "bg-gray-500" },
       { name: "Node.js", level: 70, color: "bg-green-600" },
       { name: "Express.js", level: 55, color: "bg-gray-700" },
       { name: "MongoDB", level: 60, color: "bg-green-800" },
       { name: "Firebase", level: 50, color: "bg-yellow-500" },
     ],
   },
-  {
-    title: "Tools",
-    icon: Wrench,
-    description: "Working with modern development and collaboration tools",
-    skills: [
-      { name: "Git", level: 85, color: "bg-orange-600" },
-      { name: "GitHub", level: 88, color: "bg-gray-800" },
-      { name: "VS Code", level: 90, color: "bg-blue-500" },
-      { name: "Figma", level: 75, color: "bg-pink-500" },
-      { name: "Notion", level: 70, color: "bg-gray-500" },
-    ],
-  },
-  {
-    title: "Soft Skills",
-    icon: Users,
-    description: "Personal qualities that support teamwork and growth",
-    skills: [
-      { name: "Problem Solving", level: 85, color: "bg-indigo-600" },
-      { name: "Communication", level: 80, color: "bg-purple-500" },
-      { name: "Adaptability", level: 78, color: "bg-teal-500" },
-      { name: "Collaboration", level: 82, color: "bg-pink-400" },
-      { name: "Continuous Learning", level: 90, color: "bg-green-500" },
-    ],
-  },
 ];
 
 export const TECH_STACK: TechItem[] = [
   { name: "JavaScript", icon: SiJavascript, color: "text-yellow-500" },
+  { name: "TypeScript", icon: SiTypescript, color: "text-blue-700" },
+  { name: "React", icon: SiReact, color: "text-cyan-500" },
+  { name: "TailwindCSS", icon: SiTailwindcss, color: "text-teal-400" },
   { name: "HTML5", icon: SiHtml5, color: "text-orange-500" },
   { name: "CSS3", icon: SiCss3, color: "text-blue-500" },
-
   { name: "Vite", icon: VscRocket, color: "text-purple-500" },
 ];
 
 export const PROJECTS: Project[] = [
   {
     id: 1,
-    title: "HackerNews Portal",
+    title: "HackerNews search",
     image: project1,
     description:
-      "A modern HackerNews web built with React, TailwindCSS, and Framer Motion for dynamic UI.",
+      "A modern HackerNews Search web built with React, TailwindCSS, and Framer Motion for dynamic UI.",
     tag: ["React", "TailwindCSS", "FramerMotion", "HackerNews API"],
     liveUrl: "#",
     gitHubUrl: "#",
@@ -167,8 +180,8 @@ export const PROJECTS: Project[] = [
     title: "AI Resume Studio",
     image: project2,
     description:
-      "An all-in-one AI-powered platform to create professional resumes and instantly rate them against industry standards.",
-    tag: ["React", "TailwindCSS", "TypeScript", "FramerMotion", "OpenAI API"],
+      "An all-in-one AI-powered platform to create professional resumes against industry standards.",
+    tag: ["React", "TailwindCSS", "TypeScript", "FramerMotion"],
     liveUrl: "#",
     gitHubUrl: "#",
     category: "AI Tools",
@@ -190,47 +203,38 @@ export const PROJECTS: Project[] = [
 
 export const JOURNEY_STEPS: JourneyStep[] = [
   {
-    year: "early 2025",
+    year: "Nov-2024",
     title: "Started exploring coding journey",
     company: "self-taught",
     description:
-      "Began experimenting with HTML out of curiosity and quickly discovered a passion for building on the web, inspiring a decision to pursue coding full-time.",
+      "Experimented with HTML out of curiosity and quickly discovered a passion for building on the web, inspiring a decision to pursue coding full-time.",
     icon: Briefcase,
     color: "bg-green-500",
   },
   {
-    year: "mid 2025",
+    year: "Mar-2025",
     title: "Frontend development journey",
     company: "self-taught",
     description:
-      "Started Transitioning into structured learning with React, TypeScript, and TailwindCSS, focusing on creating responsive interfaces and modern web applications.",
+      "Transitioned into structured learning with React, TypeScript, and TailwindCSS, focusing on creating responsive interfaces and modern web applications.",
     icon: GraduationCap,
     color: "bg-pink-500",
   },
   {
-    year: "late 2025",
-    title: "Making projects",
-    company: "community",
+    year: "Late-2025",
+    title: "Projects & Portfolio",
+    company: "community / self-taught",
     description:
-      "Started making projects and gaining experience in code, issue tracking, and  problem-solving.",
-    icon: ChevronsLeftRightEllipsis,
-    color: "bg-purple-500",
-  },
-  {
-    year: "late 2025",
-    title: "Portfolio & Personal Branding",
-    company: "self-taught",
-    description:
-      "Built and refined a personal portfolio from scratch, emphasizing accessibility, recruiter appeal, and cohesive branding with custom thumbnails, gradients, and a unique 'L' logo.",
+      "Built multiple projects, gained experience in problem-solving, and refined a personal portfolio emphasizing accessibility, recruiter appeal, and cohesive branding.",
     icon: User,
     color: "bg-blue-500",
   },
   {
-    year: "future",
+    year: "Future",
     title: "Professional Opportunities",
     company: "aspiring",
     description:
-      "Looking forward to apply my skills in real-world projects, joining a team where I can grow as a frontend developer, and continue crafting meaningful digital experiences.",
+      "Excited to apply my skills in real-world projects, join a team, and continue crafting meaningful digital experiences.",
     icon: Rocket,
     color: "bg-yellow-500",
   },
@@ -241,19 +245,25 @@ export const PASSION: Passion[] = [
     icon: Heart,
     title: "User Experience",
     description:
-      "Designing clean, intuitive interfaces that delight users and enhance usability.",
+      "Crafting clean, responsive interfaces from Figma to code, ensuring usability and visual appeal.",
   },
   {
     icon: Coffee,
     title: "Problem Solving",
     description:
-      "Transforming complex challenges into simple, elegant, and practical solutions.",
+      "Breaking down complex challenges into practical solutions with clear logic and maintainable code.",
   },
   {
     icon: BookOpen,
     title: "Continuous Learning",
     description:
-      "Exploring new technologies and best practices to stay adaptable and innovative.",
+      "Exploring new frameworks and best practices to stay adaptable, innovative, and future-ready.",
+  },
+  {
+    icon: Users,
+    title: "Collaboration",
+    description:
+      "Excited to contribute in team environments, share ideas openly, and learn from peers while building meaningful products.",
   },
 ];
 
